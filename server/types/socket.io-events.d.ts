37bc0beb030a4e7a977381d63071d8ba79309218
Cherepanov Kirill <cherkirr@gmail.com>
2022-07-30 14:31:40 +0300

    feat: install all the necessary packages

---
+++ server/types/socket.io-events.d.ts
@@ -0,0 +1,39 @@
+interface ServerToClientEvents {
+  enterSuccess: () => void;
+  enterFailure: (message: string) => void;
+  sessionsupdate: (sessionData: SessionData[]) => void;
+  invite: (inviter: string) => void; // Here we send socketID of the inviter
+  startGame: (opponent: string, xTurn: boolean) => void;
+
+  // changeMoveTime: (time: number) => void;
+  pause: (time: number) => void;
+  move: (position: number) => void;
+  timeIsUp: (isDefeat: boolean) => void;
+  restartMatch: () => void;
+  gameOver: () => void;
+}
+
+interface ClientToServerEvents {
+  enter: (username: string) => void;
+  invite: (invited: string) => void; // Here we send socketID of the invited
+  acceptInvite: (inviter: string) => void;
+  cancelInvite: (inviter: string, wasInvited: boolean) => void;
+
+  // changeMoveTime: (time: number) => void;
+  timeout: (time?: number) => void;
+  move: (position: number) => void;
+  restartMatch: () => void;
+  leaveGame: () => void;
+}
+
+interface InterServerEvents {}
+
+// To share information between servers
+interface SocketData {}
+
+type StandartSocket = Socket<
+  ClientToServerEvents,
+  ServerToClientEvents,
+  InterServerEvents,
+  SocketData
+>;
