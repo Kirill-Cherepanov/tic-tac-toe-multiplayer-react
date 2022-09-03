48eda6aa47eb198c5f9ec4759cac581a0d83d23d
KissMyUSSR <cherkirr@gmail.com>
2022-09-03 14:58:29 +0300

    refactor: delete fake players

--- server/src/CurrentPlayers.ts
+++ server/src/CurrentPlayers.ts
@@ -1,40 +1,6 @@
 const dbData: DbData = {
-  players: {
-    fake_id1: {
-      username: 'My Angel Kanna',
-      invited: ['fake_id1'],
-      wasInvited: [],
-      searchParams: {
-        matchTime: { value: 5, strict: true },
-        breakTime: { value: 10, strict: false }
-      }
-    },
-    fake_id2: {
-      username: 'Shiraori',
-      invited: [],
-      wasInvited: ['fake_id1'],
-      searchParams: {
-        matchTime: { value: 20, strict: false },
-        breakTime: { value: 0, strict: false }
-      }
-    }
-  },
-  games: {
-    fake_room: {
-      inviter: {
-        id: 'fake_id3',
-        username: 'KissMy'
-      },
-      invitee: {
-        id: 'fake_id4',
-        username: 'Kot-payk'
-      },
-      currentBoard: ['', '', '', '', '', '', '', '', ''],
-      currentMove: 'Kot-payk',
-      matchTime: 10,
-      breakTime: 5
-    }
-  }
+  players: {},
+  games: {}
 };
 
 export default dbData;
