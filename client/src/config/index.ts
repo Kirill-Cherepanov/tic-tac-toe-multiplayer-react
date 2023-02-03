984597a843d688c3f2c1f385aabb7b28d624a715
Cherepanov Kirill <cherkirr@gmail.com>
2023-02-03 03:58:26 +0300

    fix: client api and local addresses being mixed up

--- client/src/config/index.ts
+++ client/src/config/index.ts
@@ -4,4 +4,4 @@ const FLY_IO_BACKEND = 'https://tic-tac-toe-multiplayer.fly.dev';
 const LOCAL_BACKEND = 'http://localhost:8080';
 
 export const API_URL =
-  process.env.NODE_ENV === 'development' ? FLY_IO_BACKEND : LOCAL_BACKEND;
+  process.env.NODE_ENV === 'development' ? LOCAL_BACKEND : FLY_IO_BACKEND;
